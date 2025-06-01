67a37c20a9e21b9e8303f7ed8d12a4697aef5d56
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-02 01:43:51 +0700

    fix image

--- app/Models/StoreProduct.php
+++ app/Models/StoreProduct.php
@@ -7,6 +7,7 @@ use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class StoreProduct extends Model implements HasMedia
 {
@@ -31,14 +32,33 @@ class StoreProduct extends Model implements HasMedia
     public function getImageUrlsAttribute()
     {
         return $this->getMedia('store_product_images')->map(function ($media) {
+            // Use webp if it exists, otherwise fallback to original
+            $webpUrl = $media->hasGeneratedConversion('webp') ? $media->getUrl('webp') : $media->getUrl();
+
             return [
-                'url' => $media->getUrl(),
+                'url' => $webpUrl,
             ];
         });
     }
 
     public function getFirstImageAttribute()
     {
-        return $this->getFirstMedia('store_product_images')->getUrl();
+        $media = $this->getFirstMedia('store_product_images');
+
+        if (! $media) {
+            return null;
+        }
+
+        return $media->hasGeneratedConversion('webp')
+            ? $media->getUrl('webp')
+            : $media->getUrl();
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('webp')
+            ->format('webp')
+            ->quality(75)
+            ->nonQueued(); // remove this line if using queue
     }
 }
