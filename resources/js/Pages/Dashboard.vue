4b03716be9e77b83b3a59d0f6dd0fdb21c068aa8
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-01 23:40:48 +0700

    dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
 </script>
 
 <template>
@@ -13,8 +13,8 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg h-screen p-3">
+                    <VBtn color="primary" @click="router.get('/scanner')">Scanner</VBtn>
                 </div>
             </div>
         </div>
