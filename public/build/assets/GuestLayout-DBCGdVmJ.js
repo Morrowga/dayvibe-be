e72748707c8005d859fd4a49d2ecf0e580424d84
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-01 23:04:11 +0700

    build folder

---
+++ public/build/assets/GuestLayout-DBCGdVmJ.js
@@ -0,0 +1,1 @@
+import{o as s,f as t,b as o,j as a}from"./app-CiMnN0bz.js";const r={class:"min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"},c={class:"w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"},d={__name:"GuestLayout",setup(n){return(e,l)=>(s(),t("div",r,[o("div",c,[a(e.$slots,"default")])]))}};export{d as _};
