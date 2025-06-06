6fd21b7e79ea8f47bfc3a237042b9183dc245353
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-06 20:22:38 +0700

    size

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Size extends Model
 {
-    use HasFactory, UUID;
+    use HasFactory;
 
     protected $fillable = ['name', 'price'];
 
