31dd38936bb9a432aecf8bea2fde5d5dba3ad0a0
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-08 17:03:10 +0700

    media

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -5,11 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class MediaController extends Controller
 {
     public function show(Media $media)
     {
-        return response()->file($media->getUrl('webp'));
+        $disk = Storage::disk('s3');
+
+        if (! $disk->exists($media->getPath('webp'))) {
+            abort(404, 'File not found');
+        }
+
+        $stream = $disk->readStream($media->getPath('webp'));
+
+        return new StreamedResponse(function () use ($stream) {
+            fpassthru($stream);
+        }, 200, [
+            'Content-Type' => $media->mime_type ?? 'application/octet-stream',
+            'Content-Disposition' => 'inline; filename="' . $media->file_name . '"',
+        ]);
     }
 }
